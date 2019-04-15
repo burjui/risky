b1f26eb4d5b6c0c05f3ecde63f7776f67f1d5f22
Artem Borisovskiy <bytefu@gmail.com>
2019-04-15 16:25:08 +0200

    Version 0.2.0
    * Improve error messages
    * Change order of store instructions' arguments
    * Fix encodings of jump instructions

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 #![allow(clippy::unreadable_literal)]
 
 use std::collections::HashSet;
-use std::error::Error;
 use std::fmt;
-use std::ops::BitAnd;
 use std::ops::Range;
+use std::fmt::Display;
 
 pub mod instructions {
     use crate::csr_instruction;
@@ -17,43 +16,42 @@ pub mod instructions {
     use crate::r_instruction;
     use crate::opcode;
     use crate::parse_fence_mask;
-    use crate::shift_instruction;
     use crate::Result;
 
     macro_rules! instruction {
         (R: $name: ident, $opcode: path, funct3 $funct3: literal, funct7 $funct7: literal) => {
             pub fn $name(rd: u8, rs1: u8, rs2: u8) -> Result<u32> {
-                r_instruction($opcode, rd, $funct3, rs1, rs2, $funct7)
+                r_instruction(stringify!($name), $opcode, rd, $funct3, rs1, rs2, $funct7)
             }
         };
 
         (I: $name: ident, $opcode: path, funct3 $funct3: literal) => {
-            pub fn $name(rd: u8, rs1: u8, imm: u16) -> Result<u32> {
-                i_instruction($opcode, rd, $funct3, rs1, imm)
+            pub fn $name(rd: u8, rs1: u8, imm: i16) -> Result<u32> {
+                i_instruction(stringify!($name), $opcode, rd, $funct3, rs1, imm)
             }
         };
 
         (S: $name: ident, $opcode: path, funct3 $funct3: literal) => {
-            pub fn $name(imm: u16, rs1: u8, rs2: u8) -> Result<u32> {
-                s_instruction($opcode, imm, $funct3, rs1, rs2)
+            pub fn $name(rs1: u8, imm: i16, rs2: u8) -> Result<u32> {
+                s_instruction(stringify!($name), $opcode, imm, $funct3, rs1, rs2)
             }
         };
 
         (B: $name: ident, $opcode: path, funct3 $funct3: literal) => {
-            pub fn $name(imm: u16, rs1: u8, rs2: u8) -> Result<u32> {
-                b_instruction($opcode, imm, $funct3, rs1, rs2)
+            pub fn $name(imm: i16, rs1: u8, rs2: u8) -> Result<u32> {
+                b_instruction(stringify!($name), $opcode, imm, $funct3, rs1, rs2)
             }
         };
 
         (U: $name: ident, $opcode: path) => {
-            pub fn $name(rd: u8, imm: u32) -> Result<u32> {
-                u_instruction($opcode, rd, imm)
+            pub fn $name(rd: u8, imm: i32) -> Result<u32> {
+                u_instruction(stringify!($name), $opcode, rd, imm)
             }
         };
 
         (J: $name: ident, $opcode: path) => {
-            pub fn $name(rd: u8, imm: u32) -> Result<u32> {
-                j_instruction($opcode, rd, imm)
+            pub fn $name(rd: u8, imm: i32) -> Result<u32> {
+                j_instruction(stringify!($name), $opcode, rd, imm)
             }
         };
     }
@@ -99,45 +97,45 @@ pub mod instructions {
     instruction!(I: csrrc, opcode::SYSTEM, funct3 0b011);
 
     pub fn slli(rd: u8, rs1: u8, shamt: u8) -> Result<u32> {
-        shift_instruction(opcode::OP_IMM, rd, 0b001, rs1, shamt, 0b0000000)
+        r_instruction("slli", opcode::OP_IMM, rd, 0b001, rs1, shamt, 0b0000000)
     }
 
     pub fn srli(rd: u8, rs1: u8, shamt: u8) -> Result<u32> {
-        shift_instruction(opcode::OP_IMM, rd, 0b101, rs1, shamt, 0b0000000)
+        r_instruction("srli", opcode::OP_IMM, rd, 0b101, rs1, shamt, 0b0000000)
     }
 
     pub fn srai(rd: u8, rs1: u8, shamt: u8) -> Result<u32> {
-        shift_instruction(opcode::OP_IMM, rd, 0b101, rs1, shamt, 0b0100000)
+        r_instruction("srai", opcode::OP_IMM, rd, 0b101, rs1, shamt, 0b0100000)
     }
 
     pub fn fence(pred: &'static str, succ: &'static str) -> Result<u32> {
         let pred = parse_fence_mask(pred)?;
         let succ = parse_fence_mask(succ)?;
-        fence_instruction(0b000, pred, succ)
+        fence_instruction("fence", 0b000, pred, succ)
     }
 
     pub fn fence_i() -> Result<u32> {
-        fence_instruction(0b001, 0b0000, 0b0000)
+        fence_instruction("fence_i", 0b001, 0b0000, 0b0000)
     }
 
     pub fn ecall() -> Result<u32> {
-        i_instruction(opcode::SYSTEM, 0b00000, 0b000, 0b00000, 0b0000_0000_0000)
+        i_instruction("ecall", opcode::SYSTEM, 0b00000, 0b000, 0b00000, 0b0000_0000_0000)
     }
 
     pub fn ebreak() -> Result<u32> {
-        i_instruction(opcode::SYSTEM, 0b00000, 0b000, 0b00000, 0b0000_0000_0001)
+        i_instruction("ebreak", opcode::SYSTEM, 0b00000, 0b000, 0b00000, 0b0000_0000_0001)
     }
 
     pub fn csrrwi(rd: u8, rs1: u8, csr: u16) -> Result<u32> {
-        csr_instruction(rd, 0b101, rs1, csr)
+        csr_instruction("csrrwi", rd, 0b101, rs1, csr)
     }
 
     pub fn csrrsi(rd: u8, rs1: u8, csr: u16) -> Result<u32> {
-        csr_instruction(rd, 0b110, rs1, csr)
+        csr_instruction("csrrsi", rd, 0b110, rs1, csr)
     }
 
     pub fn csrrci(rd: u8, rs1: u8, csr: u16) -> Result<u32> {
-        csr_instruction(rd, 0b111, rs1, csr)
+        csr_instruction("csrrci", rd, 0b111, rs1, csr)
     }
 
     // RV32M Standard Extension
@@ -187,95 +185,50 @@ mod opcode {
     );
 }
 
-pub type Result<T> = std::result::Result<T, RISCVError>;
+pub type Result<T> = std::result::Result<T, String>;
 
-#[derive(Debug)]
-pub enum RISCVError {
-    OutOfRange {
-        value: u32,
-        range: Range<u32>,
-        name: &'static str,
-    },
-    NotMultipleOfTwo {
-        value: u32,
-        name: &'static str,
-    },
-    FenceFlag {
-        value: &'static str,
-        name: char,
-        kind: &'static str,
-    },
+fn fence_instruction(function_name: &'static str, funct3: u8, pred: u8, succ: u8) -> Result<u32> {
+    check_funct3(function_name, funct3)?;
+    check_range(function_name, "pred", pred, 0 .. 1 << 4)?;
+    check_range(function_name, "succ", succ, 0 .. 1 << 4)?;
+    let imm = i16::from(succ | pred << 4);
+    i_instruction(function_name, opcode::MISC_MEM, 0x00000, 0b00, 0x00000, imm)
 }
 
-impl fmt::Display for RISCVError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            RISCVError::OutOfRange { value, range, name } =>
-                write!(f, "{} = {} (0x{:08x}) is out of range: {} .. {} (0x{:08x} .. (0x{:08x}))",
-                       name, value, value, range.start, range.end, range.start, range.end),
-            RISCVError::NotMultipleOfTwo { value, name } =>
-                write!(f, "{} = {} (0x{:08x}) is not a multiple of 2", name, value, value),
-            RISCVError::FenceFlag { value, name, kind } =>
-                write!(f, "{} fence flag `{}' in fence mask `{}'", kind, name, value),
-        }
-    }
-}
-
-impl Error for RISCVError {}
-
-fn shift_instruction(opcode: u8, rd: u8, funct3: u8, rs1: u8, shamt: u8, funct7: u8) -> Result<u32> {
-    check_range(shamt, 0 .. 1 << 5, "shamt")?;
-    check_funct7(funct7)?;
-    let imm = u16::from(shamt) | u16::from(funct7) << 5;
-    i_instruction(opcode, rd, funct3, rs1, imm)
-}
-
-fn fence_instruction(funct3: u8, pred: u8, succ: u8) -> Result<u32> {
-    check_funct3(funct3)?;
-    check_range(pred, 0 .. 1 << 4, "pred")?;
-    check_range(succ, 0 .. 1 << 4, "pred")?;
-    let imm = u16::from(succ) | u16::from(pred) << 4;
-    i_instruction(opcode::MISC_MEM, 0x00000, 0b00, 0x00000, imm)
-}
-
-fn parse_fence_mask(s: &'static str) -> Result<u8> {
+fn parse_fence_mask(mask_str: &'static str) -> Result<u8> {
     let mut chars_processed = HashSet::new();
     let mut mask = 0;
-    for c in s.chars() {
-        if chars_processed.contains(&c) {
-            return Err(RISCVError::FenceFlag {
-                value: s,
-                name: c,
-                kind: "duplicate",
-            });
+    for flag_name in mask_str.chars() {
+        if chars_processed.contains(&flag_name) {
+            return Err(fence_flag_error("unknown", flag_name, mask_str));
         }
-        chars_processed.insert(c);
-        mask |= match c {
+        chars_processed.insert(flag_name);
+        mask |= match flag_name {
             'i' => 1 << 3,
             'o' => 1 << 2,
             'r' => 1 << 1,
             'w' => 1,
-            _ => return Err(RISCVError::FenceFlag {
-                value: s,
-                name: c,
-                kind: "invalid",
-            }),
+            _ => return Err(fence_flag_error("invalid", flag_name, mask_str)),
         };
     }
     Ok(mask)
 }
 
-fn csr_instruction(rd: u8, funct3: u8, rs1: u8, csr: u16) -> Result<u32> {
-    i_instruction(opcode::SYSTEM, rd, funct3, rs1, csr)
+fn fence_flag_error(kind: &'static str, flag_name: char, mask_str: &str) -> String {
+    format!("{} fence flag name `{}' in fence mask `{}'", kind, flag_name, mask_str)
 }
 
-fn r_instruction(opcode: u8, rd: u8, funct3: u8, rs1: u8, rs2: u8, funct7: u8) -> Result<u32> {
-    check_opcode(opcode)?;
-    check_register(rd)?;
-    check_funct3(funct3)?;
-    check_register(rs1)?;
-    check_register(rs2)?;
-    check_funct7(funct7)?;
+fn csr_instruction(function_name: &'static str, rd: u8, funct3: u8, rs1: u8, csr: u16) -> Result<u32> {
+    i_instruction(function_name, opcode::SYSTEM, rd, funct3, rs1, csr as i16)
+}
+
+fn r_instruction(function_name: &'static str, opcode: u8, rd: u8, funct3: u8, rs1: u8, rs2: u8, funct7: u8) -> Result<u32> {
+    check_opcode(function_name, opcode)?;
+    check_register(function_name, rd)?;
+    check_funct3(function_name, funct3)?;
+    check_register(function_name, rs1)?;
+    check_register(function_name, rs2)?;
+    check_funct7(function_name, funct7)?;
     Ok(u32::from(opcode) |
         (u32::from(rd) << 7) |
         (u32::from(funct3) << 12) |
@@ -284,123 +237,114 @@ fn r_instruction(opcode: u8, rd: u8, funct3: u8, rs1: u8, rs2: u8, funct7: u8) -
         (u32::from(funct7) << 25))
 }
 
-fn i_instruction(opcode: u8, rd: u8, funct3: u8, rs1: u8, imm: u16) -> Result<u32> {
-    check_opcode(opcode)?;
-    check_register(rd)?;
-    check_funct3(funct3)?;
-    check_imm_i_s(imm)?;
+fn i_instruction(function_name: &'static str, opcode: u8, rd: u8, funct3: u8, rs1: u8, imm: i16) -> Result<u32> {
+    check_opcode(function_name, opcode)?;
+    check_register(function_name, rd)?;
+    check_funct3(function_name, funct3)?;
+    check_imm_i_s(function_name, imm)?;
     Ok(u32::from(opcode) |
         (u32::from(rd) << 7) |
         (u32::from(funct3) << 12) |
         (u32::from(rs1) << 15) |
-        (u32::from(imm) << 20))
+        ((imm as u32) << 20))
 }
 
-fn s_instruction(opcode: u8, imm: u16, funct3: u8, rs1: u8, rs2: u8) -> Result<u32> {
-    check_opcode(opcode)?;
-    check_imm_i_s(imm)?;
-    check_funct3(funct3)?;
-    check_register(rs1)?;
-    check_register(rs2)?;
+fn s_instruction(function_name: &'static str, opcode: u8, imm: i16, funct3: u8, rs1: u8, rs2: u8) -> Result<u32> {
+    check_opcode(function_name, opcode)?;
+    check_imm_i_s(function_name, imm)?;
+    check_funct3(function_name, funct3)?;
+    check_register(function_name, rs1)?;
+    check_register(function_name, rs2)?;
+    let imm_u32 = imm as u32;
     Ok(u32::from(opcode) |
-        (u32::from(imm & 0b11111) << 7) |
-        (u32::from(funct3) << 12) |
-        (u32::from(rs1) << 15) |
-        (u32::from(rs2) << 20) |
-        (u32::from((imm >> 5) & 0b111_1111) << 25))
-}
-
-fn b_instruction(opcode: u8, imm: u16, funct3: u8, rs1: u8, rs2: u8) -> Result<u32> {
-    check_opcode(opcode)?;
-    check_imm_b(imm)?;
-    check_funct3(funct3)?;
-    check_register(rs1)?;
-    check_register(rs2)?;
-    Ok(u32::from(opcode) |
-        (u32::from((imm >> 11) & 0b1) << 7) |
-        (u32::from((imm >> 1) & 0b1111) << 8) |
+        ((imm_u32 & 0b11111) << 7) |
         (u32::from(funct3) << 12) |
         (u32::from(rs1) << 15) |
         (u32::from(rs2) << 20) |
-        (u32::from((imm >> 5) & 0b11_1111) << 25) |
-        (u32::from((imm >> 12) & 0b1) << 31))
+        ((imm_u32 >> 5) & 0b111_1111) << 25)
 }
 
-fn u_instruction(opcode: u8, rd: u8, imm: u32) -> Result<u32> {
-    check_opcode(opcode)?;
-    check_register(rd)?;
-    check_imm_u(imm)?;
+fn b_instruction(function_name: &'static str, opcode: u8, imm: i16, funct3: u8, rs1: u8, rs2: u8) -> Result<u32> {
+    check_opcode(function_name, opcode)?;
+    check_imm_b(function_name, imm)?;
+    check_funct3(function_name, funct3)?;
+    check_register(function_name, rs1)?;
+    check_register(function_name, rs2)?;
+    let imm_u32 = imm as u32;
+    Ok(u32::from(opcode) |
+        (u32::from((imm_u32 >> 11) & 0b1) << 7) |
+        (u32::from((imm_u32 >> 1) & 0b1111) << 8) |
+        (u32::from(funct3) << 12) |
+        (u32::from(rs1) << 15) |
+        (u32::from(rs2) << 20) |
+        (u32::from((imm_u32 >> 5) & 0b11_1111) << 25) |
+        (u32::from((imm_u32 >> 12) & 0b1) << 31))
+}
+
+fn u_instruction(function_name: &'static str, opcode: u8, rd: u8, imm: i32) -> Result<u32> {
+    check_opcode(function_name, opcode)?;
+    check_register(function_name, rd)?;
     Ok(u32::from(opcode) |
         (u32::from(rd) << 7) |
-        (imm << 12))
+        (imm as u32 & 0xFFFF_F000))
 }
 
-fn j_instruction(opcode: u8, rd: u8, imm: u32) -> Result<u32> {
-    check_opcode(opcode)?;
-    check_register(rd)?;
-    let imm =
-        (imm >> 12) & 0b1111_1111 |
-            ((imm >> 11) & 0b1) << 8 |
-            ((imm >> 1) & 0b11_1111_1111) << 9 |
-            ((imm >> 20) & 0b1) << 19;
-    check_imm_u(imm)?;
-    Ok(u32::from(opcode) |
-        (u32::from(rd) << 7) |
-        (imm << 12))
+fn j_instruction(function_name: &'static str, opcode: u8, rd: u8, imm: i32) -> Result<u32> {
+    check_opcode(function_name, opcode)?;
+    check_register(function_name, rd)?;
+    check_imm_j(function_name, imm)?;
+    let imm_u32 = imm as u32;
+    let imm_field =
+        (imm_u32 >> 12 & 0b1111_1111) << 12 |
+        (imm_u32 >> 11 & 0b1) << 20 |
+        (imm_u32 >> 1 & 0b11_1111_1111) << 21 |
+        (imm_u32 >> 20 & 0b1) << 31;
+    Ok(u32::from(opcode) | (u32::from(rd) << 7) | imm_field)
 }
 
-fn check_opcode(opcode: u8) -> Result<()> {
-    check_range(opcode, 0b11 .. 1 << 7, "opcode")
+fn check_opcode(function_name: &'static str, opcode: u8) -> Result<()> {
+    check_range(function_name, "opcode", opcode, 0b11 .. 1 << 7)
 }
 
-fn check_register(register: u8) -> Result<()> {
-    check_range(register, 0 .. 1 << 5, "register")
+fn check_register(function_name: &'static str, register: u8) -> Result<()> {
+    check_range(function_name, "register", register, 0 .. 1 << 5)
 }
 
-fn check_imm_i_s(imm: u16) -> Result<()> {
-    check_range(imm, 0 .. 1 << 12, "imm")
+fn check_imm_i_s(function_name: &'static str, imm: i16) -> Result<()> {
+    check_range(function_name, "imm", imm, -(1 << 12) .. 1 << 12)
 }
 
-fn check_imm_b(imm: u16) -> Result<()> {
-    check_range(imm, 0 .. 1 << 13, "imm")?;
-    check_multiple_of_two(imm, "i")
+fn check_imm_b(function_name: &'static str, imm: i16) -> Result<()> {
+    check_range(function_name, "imm", imm, -(1 << 13) .. 1 << 13)
+        .and_then(|()| match imm & 1 {
+            0 => Ok(()),
+            _ => Err(format!("imm = {} (0x{:08x}) is not a multiple of 2", imm, imm))
+        })
 }
 
-fn check_imm_u(imm: u32) -> Result<()> {
-    check_range(imm, 0 .. 1 << 20, "imm")
+fn check_imm_j(function_name: &'static str, imm: i32) -> Result<()> {
+    check_range(function_name, "imm", imm, -(1 << 21) .. 1 << 21)
+        .and_then(|()| match imm & 1 {
+            0 => Ok(()),
+            _ => Err(format!("imm = {} (0x{:08x}) is not a multiple of 2", imm, imm))
+        })
 }
 
-fn check_funct3(funct3: u8) -> Result<()> {
-    check_range(funct3, 0 .. 1 << 3, "funct3")
+fn check_funct3(function_name: &'static str, funct3: u8) -> Result<()> {
+    check_range(function_name, "funct3", funct3, 0 .. 1 << 3)
 }
 
-fn check_funct7(funct7: u8) -> Result<()> {
-    check_range(funct7, 0 .. 1 << 7, "funct7")
+fn check_funct7(function_name: &'static str, funct7: u8) -> Result<()> {
+    check_range(function_name, "funct7", funct7, 0 .. 1 << 7)
 }
 
-fn check_range<T: PartialOrd>(value: T, range: Range<T>, name: &'static str) -> Result<()>
-    where u32: std::convert::From<T>
+fn check_range<T>(function_name: &'static str, value_name: &'static str, value: T, range: Range<T>) -> Result<()>
+    where T: PartialOrd + Display + fmt::LowerHex
 {
     if value >= range.start && value < range.end {
         Ok(())
     } else {
-        Err(RISCVError::OutOfRange {
-            value: u32::from(value),
-            range: u32::from(range.start) .. u32::from(range.end),
-            name,
-        })
-    }
-}
-
-fn check_multiple_of_two<T>(value: T, name: &'static str) -> Result<()>
-    where T: BitAnd + PartialEq,
-          u32: std::convert::From<T>,
-          u32: std::convert::From<T>,
-{
-    let value = u32::from(value);
-    if value & 0b1 == 0 {
-        Ok(())
-    } else {
-        Err(RISCVError::NotMultipleOfTwo { value, name })
+        Err(format!("{}: {} = {} (0x{:08x}) is out of range: {} .. {} (0x{:08x} .. (0x{:08x}))",
+            function_name, value_name, value, value, range.start, range.start, range.end, range.end))
     }
 }
