d0b8339262603e8d22e43aa728f66ce3893b1dc0
Artem Borisovskiy <bytefu@gmail.com>
2022-12-02 00:46:40 +0200

    Additional conversions and tests for BImm

--- src/instructions/b_imm.rs
+++ src/instructions/b_imm.rs
@@ -11,6 +11,10 @@ use crate::util::{
     i32_fits_n_bits,
     i64_fits_n_bits,
     isize_fits_n_bits,
+    u16_fits_n_bits,
+    u32_fits_n_bits,
+    u64_fits_n_bits,
+    usize_fits_n_bits,
 };
 
 /// 13-bit signed immediate value used in branch instructions
@@ -29,9 +33,14 @@ impl BImm {
         Self(VALUE as i16 & !1)
     }
 
+    /// Creates an `BImm` from an [u8] constant
+    pub const fn from_u8<const VALUE: u8>() -> Self {
+        Self(VALUE as i16 & !1)
+    }
+
     #[doc = include_str!("../../doc/nightly_warning.html")]
     ///
-    /// Creates an `BImm` from an [i16] constant
+    /// Creates an `BImm` from an [i16] constant, failing to compile if the constant does not fit 13-bit signed range
     #[cfg(feature = "nightly")]
     #[must_use]
     pub const fn from_i16<const VALUE: i16>() -> Self
@@ -43,7 +52,7 @@ impl BImm {
 
     #[doc = include_str!("../../doc/nightly_warning.html")]
     ///
-    /// Creates an `BImm` from an [i32] constant
+    /// Creates an `BImm` from an [i32] constant, failing to compile if the constant does not fit 13-bit signed range
     #[cfg(feature = "nightly")]
     #[must_use]
     pub const fn from_i32<const VALUE: i32>() -> Self
@@ -55,12 +64,72 @@ impl BImm {
 
     #[doc = include_str!("../../doc/nightly_warning.html")]
     ///
-    /// Creates an `BImm` from an [i64] constant
+    /// Creates an `BImm` from an [i64] constant, failing to compile if the constant does not fit 13-bit signed range
     #[cfg(feature = "nightly")]
     #[must_use]
     pub const fn from_i64<const VALUE: i64>() -> Self
     where
         internal::Assert<{ i64_fits_n_bits(VALUE, Self::NBITS) }>: internal::Fits13BIts,
+    {
+        Self(VALUE as i16 & !1)
+    }
+
+    #[doc = include_str!("../../doc/nightly_warning.html")]
+    ///
+    /// Creates an `BImm` from an [isize] constant, failing to compile if the constant does not fit 13-bit signed range
+    #[cfg(feature = "nightly")]
+    #[must_use]
+    pub const fn from_isize<const VALUE: isize>() -> Self
+    where
+        internal::Assert<{ isize_fits_n_bits(VALUE, Self::NBITS) }>: internal::Fits13BIts,
+    {
+        Self(VALUE as i16 & !1)
+    }
+
+    #[doc = include_str!("../../doc/nightly_warning.html")]
+    ///
+    /// Creates an `BImm` from an [u16] constant, failing to compile if the constant does not fit 13-bit signed range
+    #[cfg(feature = "nightly")]
+    #[must_use]
+    pub const fn from_u16<const VALUE: u16>() -> Self
+    where
+        internal::Assert<{ u16_fits_n_bits(VALUE, Self::NBITS - 1) }>: internal::Fits13BIts,
+    {
+        Self(VALUE as i16 & !1)
+    }
+
+    #[doc = include_str!("../../doc/nightly_warning.html")]
+    ///
+    /// Creates an `BImm` from an [u32] constant, failing to compile if the constant does not fit 13-bit signed range
+    #[cfg(feature = "nightly")]
+    #[must_use]
+    pub const fn from_u32<const VALUE: u32>() -> Self
+    where
+        internal::Assert<{ u32_fits_n_bits(VALUE, Self::NBITS - 1) }>: internal::Fits13BIts,
+    {
+        Self(VALUE as i16 & !1)
+    }
+
+    #[doc = include_str!("../../doc/nightly_warning.html")]
+    ///
+    /// Creates an `BImm` from an [u64] constant, failing to compile if the constant does not fit 13-bit signed range
+    #[cfg(feature = "nightly")]
+    #[must_use]
+    pub const fn from_u64<const VALUE: u64>() -> Self
+    where
+        internal::Assert<{ u64_fits_n_bits(VALUE, Self::NBITS - 1) }>: internal::Fits13BIts,
+    {
+        Self(VALUE as i16 & !1)
+    }
+
+    #[doc = include_str!("../../doc/nightly_warning.html")]
+    ///
+    /// Creates an `BImm` from an [usize] constant, failing to compile if the constant does not fit 13-bit signed range
+    #[cfg(feature = "nightly")]
+    #[must_use]
+    pub const fn from_usize<const VALUE: usize>() -> Self
+    where
+        internal::Assert<{ usize_fits_n_bits(VALUE, Self::NBITS - 1) }>: internal::Fits13BIts,
     {
         Self(VALUE as i16 & !1)
     }
@@ -75,12 +144,19 @@ impl BImm {
 fn constructors() {
     let _ = BImm::from_i8::<-128>();
     let _ = BImm::from_i8::<127>();
+    let _ = BImm::from_u8::<255>();
     let _ = BImm::from_i16::<-4096>();
     let _ = BImm::from_i16::<4095>();
+    let _ = BImm::from_u16::<4095>();
     let _ = BImm::from_i32::<-4096>();
     let _ = BImm::from_i32::<4095>();
+    let _ = BImm::from_u32::<4095>();
     let _ = BImm::from_i64::<-4096>();
     let _ = BImm::from_i64::<4095>();
+    let _ = BImm::from_u64::<4095>();
+    let _ = BImm::from_isize::<-4096>();
+    let _ = BImm::from_isize::<4095>();
+    let _ = BImm::from_usize::<4095>();
 }
 
 impl Display for BImm {
@@ -98,6 +174,12 @@ fn display() -> Result<(), BImmConvError> {
 
 impl From<i8> for BImm {
     fn from(value: i8) -> Self {
+        Self(value as i16 & !1)
+    }
+}
+
+impl From<u8> for BImm {
+    fn from(value: u8) -> Self {
         Self(value as i16 & !1)
     }
 }
@@ -149,40 +231,125 @@ impl TryFrom<isize> for BImm {
     }
 }
 
+impl TryFrom<u16> for BImm {
+    type Error = BImmConvError;
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        if u16_fits_n_bits(value, Self::NBITS - 1) {
+            Ok(Self(value as i16 & !1))
+        } else {
+            Err(BImmConvError::U16(value))
+        }
+    }
+}
+
+impl TryFrom<u32> for BImm {
+    type Error = BImmConvError;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        if u32_fits_n_bits(value, Self::NBITS - 1) {
+            Ok(Self(value as i16 & !1))
+        } else {
+            Err(BImmConvError::U32(value))
+        }
+    }
+}
+
+impl TryFrom<u64> for BImm {
+    type Error = BImmConvError;
+
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        if u64_fits_n_bits(value, Self::NBITS - 1) {
+            Ok(Self(value as i16 & !1))
+        } else {
+            Err(BImmConvError::U64(value))
+        }
+    }
+}
+
+impl TryFrom<usize> for BImm {
+    type Error = BImmConvError;
+
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        if usize_fits_n_bits(value, Self::NBITS - 1) {
+            Ok(Self(value as i16 & !1))
+        } else {
+            Err(BImmConvError::Usize(value))
+        }
+    }
+}
+
 #[test]
 fn conversions() -> Result<(), BImmConvError> {
     assert_eq!(BImm::from(-128_i8), BImm(-128));
     assert_eq!(BImm::from(127_i8), BImm(126));
+    assert_eq!(BImm::from(255_u8), BImm(254));
+
     assert_eq!(BImm::try_from(-4096_i16)?, BImm(-4096));
     assert_eq!(BImm::try_from(4095_i16)?, BImm(4094));
+    assert!(matches!(
+        BImm::try_from(-4097_i16),
+        Err(BImmConvError::I16(-4097))
+    ));
+    assert!(matches!(
+        BImm::try_from(4096_i16),
+        Err(BImmConvError::I16(4096))
+    ));
+
+    assert_eq!(BImm::try_from(4095_u16)?, BImm(4094));
+    assert!(matches!(
+        BImm::try_from(4096_u16),
+        Err(BImmConvError::U16(4096))
+    ));
+
     assert_eq!(BImm::try_from(-4096_i32)?, BImm(-4096));
     assert_eq!(BImm::try_from(4095_i32)?, BImm(4094));
+    assert!(matches!(
+        BImm::try_from(-4097_i32),
+        Err(BImmConvError::I32(-4097))
+    ));
+    assert!(matches!(
+        BImm::try_from(4096_i32),
+        Err(BImmConvError::I32(4096))
+    ));
+
+    assert_eq!(BImm::try_from(4095_u32)?, BImm(4094));
+    assert!(matches!(
+        BImm::try_from(4096_u32),
+        Err(BImmConvError::U32(4096))
+    ));
+
     assert_eq!(BImm::try_from(-4096_i64)?, BImm(-4096));
     assert_eq!(BImm::try_from(4095_i64)?, BImm(4094));
+    assert!(matches!(
+        BImm::try_from(-4097_i64),
+        Err(BImmConvError::I64(-4097))
+    ));
+    assert!(matches!(
+        BImm::try_from(4096_i64),
+        Err(BImmConvError::I64(4096))
+    ));
 
+    assert_eq!(BImm::try_from(4095_u64)?, BImm(4094));
     assert!(matches!(
-        BImm::try_from(-1048577_i32),
-        Err(BImmConvError::I32(-1048577))
+        BImm::try_from(4096_u64),
+        Err(BImmConvError::U64(4096))
+    ));
+
+    assert_eq!(BImm::try_from(-4096_isize)?, BImm(-4096));
+    assert_eq!(BImm::try_from(4095_isize)?, BImm(4094));
+    assert!(matches!(
+        BImm::try_from(-4097_isize),
+        Err(BImmConvError::Isize(-4097))
     ));
     assert!(matches!(
-        BImm::try_from(1048576_i32),
-        Err(BImmConvError::I32(1048576))
+        BImm::try_from(4096_isize),
+        Err(BImmConvError::Isize(4096))
     ));
+
+    assert_eq!(BImm::try_from(4095_usize)?, BImm(4094));
     assert!(matches!(
-        BImm::try_from(-1048577_i64),
-        Err(BImmConvError::I64(-1048577))
-    ));
-    assert!(matches!(
-        BImm::try_from(1048576_i64),
-        Err(BImmConvError::I64(1048576))
-    ));
-    assert!(matches!(
-        BImm::try_from(-1048577_isize),
-        Err(BImmConvError::Isize(-1048577))
-    ));
-    assert!(matches!(
-        BImm::try_from(1048576_isize),
-        Err(BImmConvError::Isize(1048576))
+        BImm::try_from(4096_usize),
+        Err(BImmConvError::Usize(4096))
     ));
 
     Ok(())
@@ -199,6 +366,14 @@ pub enum BImmConvError {
     I64(i64),
     ///
     Isize(isize),
+    ///
+    U16(u16),
+    ///
+    U32(u32),
+    ///
+    U64(u64),
+    ///
+    Usize(usize),
 }
 
 impl Display for BImmConvError {
@@ -209,6 +384,10 @@ impl Display for BImmConvError {
             BImmConvError::I32(value) => write!(f, "{} (0x{:08x})", value, value),
             BImmConvError::I64(value) => write!(f, "{} (0x{:016x})", value, value),
             BImmConvError::Isize(value) => write!(f, "{}", value),
+            BImmConvError::U16(value) => write!(f, "{} (0x{:04x})", value, value),
+            BImmConvError::U32(value) => write!(f, "{} (0x{:08x})", value, value),
+            BImmConvError::U64(value) => write!(f, "{} (0x{:016x})", value, value),
+            BImmConvError::Usize(value) => write!(f, "{}", value),
         }
     }
 }
@@ -228,6 +407,15 @@ fn conv_error_impl_display() {
             BImm::NBITS
         )
     );
+
+    assert_eq!(
+        BImm::try_from(4096_u16).unwrap_err().to_string(),
+        format!(
+            "invalid {}-bit signed immediate: 4096 (0x1000)",
+            BImm::NBITS
+        )
+    );
+
     assert_eq!(
         BImm::try_from(-4097_i32).unwrap_err().to_string(),
         format!(
@@ -242,6 +430,15 @@ fn conv_error_impl_display() {
             BImm::NBITS
         )
     );
+
+    assert_eq!(
+        BImm::try_from(4096_u32).unwrap_err().to_string(),
+        format!(
+            "invalid {}-bit signed immediate: 4096 (0x00001000)",
+            BImm::NBITS
+        )
+    );
+
     assert_eq!(
         BImm::try_from(-4097_i64).unwrap_err().to_string(),
         format!(
@@ -256,12 +453,26 @@ fn conv_error_impl_display() {
             BImm::NBITS
         )
     );
+
+    assert_eq!(
+        BImm::try_from(4096_u64).unwrap_err().to_string(),
+        format!(
+            "invalid {}-bit signed immediate: 4096 (0x0000000000001000)",
+            BImm::NBITS
+        )
+    );
+
     assert_eq!(
         BImm::try_from(-4097_isize).unwrap_err().to_string(),
         format!("invalid {}-bit signed immediate: -4097", BImm::NBITS)
     );
     assert_eq!(
         BImm::try_from(4096_isize).unwrap_err().to_string(),
+        format!("invalid {}-bit signed immediate: 4096", BImm::NBITS)
+    );
+
+    assert_eq!(
+        BImm::try_from(4096_usize).unwrap_err().to_string(),
         format!("invalid {}-bit signed immediate: 4096", BImm::NBITS)
     );
 }
